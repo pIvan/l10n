1f3c1789271f433c18f1d6b9607ba08a5f0075b6
pIvan <pintar.ivan@gmail.com>
2018-03-25 18:13:49 +0200

    Update Travis CI config and @angular dependency

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,12 @@ module.exports = function (config) {
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    customLaunchers: {
+      ChromeNoSandboxHeadless: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu'],
+      }
+    },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
@@ -27,7 +33,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeNoSandboxHeadless'],
     singleRun: true
   });
 };
