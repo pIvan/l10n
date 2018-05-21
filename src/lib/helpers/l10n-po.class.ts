b9fffcd10da72519e57a54cc88a922247866bdf5
pIvan <pintar.ivan@gmail.com>
2018-05-21 22:16:10 +0200

    npm tag fix

--- src/lib/helpers/l10n-po.class.ts
+++ src/lib/helpers/l10n-po.class.ts
@@ -49,21 +49,6 @@ export class L10nPo {
         });
     }
 
-    // notice the double %%, this prints a literal '%' character
-    // printf("%%b = '%b'\n", $n); // binary representation
-    // printf("%%c = '%c'\n", $c); // print the ascii character, same as chr() function
-    // printf("%%d = '%d'\n", $n); // standard integer representation
-    // printf("%%e = '%e'\n", $n); // scientific notation
-    // printf("%%u = '%u'\n", $n); // unsigned integer representation of a positive integer
-    // printf("%%u = '%u'\n", $u); // unsigned integer representation of a negative integer
-    // printf("%%f = '%f'\n", $n); // floating point representation
-    // printf("%%o = '%o'\n", $n); // octal representation
-    // printf("%%s = '%s'\n", $n); // string representation
-    // printf("%%x = '%x'\n", $n); // hexadecimal representation (lower-case)
-    // printf("%%X = '%X'\n", $n); // hexadecimal representation (upper-case)
-
-    // printf("%%+d = '%+d'\n", $n); // sign specifier on a positive integer
-    // printf("%%+d = '%+d'\n", $u); // sign specifier on a negative integer
     private extract(text: string): string {
         return text.replace(this.blank, '')
             .replace(/^[^"]*"|"$/g, '')
